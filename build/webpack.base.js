0678c3af27c4b53a3d98a639b5f2261fa9e1c260
qq122453418 <122453418@qq.com>
2021-02-06 00:07:04 +0800

    init

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -6,41 +6,10 @@ module.exports = {
     entry: './src/entry/index.js',
     module: {
         rules:[
-            /* {
-             *     test: /\.js$/,
-             *     loader: 'babel-loader',
-             *     exclude: /node_modules/
-             * }, */
             {
                 test: /\.js$/,
                 loader: 'babel-loader',
-                exclude: /node_modules/,
-                options: {
-                    // 预设：指示babel怎么样的兼容性处理
-                    presets: [
-                        [
-                            '@babel/preset-env',
-                            {
-                                // 设置为按需加载
-                                useBuiltIns: 'usage',
-
-                                // 指定core-js版本
-                                corejs: {
-                                    version: 3
-                                },
-
-                                // 指定兼容性做到哪个版本浏览器
-                                targets: {
-                                    chrome: '60',
-                                    firefox: '60',
-                                    ie: '9',
-                                    safari: '10',
-                                    edge: '17'
-                                }
-                            }
-                        ]
-                    ]
-                }
+                exclude: /node_modules/
             },
             {
                 test: /\.vue$/,
